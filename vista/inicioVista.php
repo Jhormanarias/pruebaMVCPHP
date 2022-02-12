a4c9bdfa5acdc2b796e0428e52e023fe31dc4b0d
unknown <SENA@CARAPRCTIFSD156.FormacionSena.RED>
2022-02-12 17:05:48 -0500

    Agregar bootstrap a inicio

--- vista/inicioVista.php
+++ vista/inicioVista.php
@@ -1,24 +1,58 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
-    <link rel="stylesheet" href="vista/css/style.css">
-</head>
-<body class="bodyInicio">
-    <h1>Soy la vista inicio</h1>
-    <br><br><br>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem ducimus labore pariatur! Corporis, nostrum minima omnis a totam est, dolore veritatis et, fugiat animi suscipit quod harum quia alias?
-    Voluptates quisquam, consequatur quasi veniam maxime totam tempora modi cum sapiente, dolore ducimus voluptatem. Placeat temporibus nesciunt deleniti ducimus, assumenda sit, tempora illum iste ipsam ad velit dolor eaque impedit?
-    Molestiae quis iure, earum iusto odit rerum dolorum, aperiam maxime accusamus in sint a aliquid nesciunt eum quidem animi iste maiores voluptas nisi laboriosam. Pariatur cum sunt deserunt repellat temporibus!
-    Nihil magni quia, reprehenderit ullam aliquam ipsum numquam vero fuga impedit hic ducimus sunt illo nemo repellendus voluptatibus. Illum nostrum at id ab reprehenderit accusantium facilis dicta harum totam nesciunt!
-    Corporis modi maiores voluptates, commodi numquam doloremque iusto, nulla nesciunt distinctio dolorem fugit voluptatum error consequatur, quibusdam molestias tempore minus eum rem accusamus laboriosam. Accusantium quod deserunt porro accusamus explicabo.</p>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <a href="?c=inicio">Inicio</a>
-    <a href="?c=usuario">Usuario</a>
-    <a href="?c=Factura">Factura</a>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-</body>
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+        <div class="row mt-4">
+            <div class="col-md-6 ">
+                <div class="card" style="width: 18rem;">
+                <img src="https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2018/08/fondos-pantalla-full-hd-animales_4.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Active</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?c=usuario">Usuario</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?c=Factura">Factura</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                    </ul>
+            </div>
+
+
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
